26c8f3e42961b96e4fab1876f61061682cfa5f0b
Tasmih Rob Mow <tasmih.mow20@gmail.com>
2026-07-19 03:46:04 +0600

    Build dashboard UI, create AI Study Planner interface,create AI Tutor Chat interface and complete CRUD pages and final frontend improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Providers from "./providers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "StudyPilot AI - Your Ultimate Premium Study Companion",
@@ -21,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <Providers>
           {children}
           <ToastContainer 
